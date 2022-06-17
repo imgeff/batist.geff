9c6d984236d139ef738215496a67a23c3b33b449
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-06-17 19:00:37 -0300

    :sparkles: feat: Cria componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+function Header () {
+	return (
+		<header>
+			<div>Imgeff</div>
+			<nav>
+				<a href="">Início</a>
+				<a href="">Sobre</a>
+				<a href="">Formação</a>
+				<a href="">Habilidades</a>
+				<a href="">Projetos</a>
+				<a href="">Contato</a>
+			</nav>
+		</header>
+	);
+}
+
+export { Header };
