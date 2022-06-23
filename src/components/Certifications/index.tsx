f1ac180d22bf6d3b9c2f2da50e7733e87cc98d35
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-06-23 20:27:21 -0300

    :construction: feat: Cria componente Certifications

---
+++ src/components/Certifications/index.tsx
@@ -0,0 +1,29 @@
+import './style.css';
+
+function Certifications () {
+  return(
+    <section id="certifications-section">
+      <h1>Certificações</h1>
+      <div id="certifications-box">
+        <div className="certificate">
+          <figure>
+            <img src="" alt="" />
+          </figure>
+          <span>Trybe</span>
+          <span>Fundamentos do Desenvolvimento Web</span>
+          <a href="">Ver Certificado</a>
+        </div>
+        <div className="certificate">
+        <figure>
+            <img src="" alt="" />
+          </figure>
+          <span>Trybe</span>
+          <span>Fundamentos do Desenvolvimento Web</span>
+          <a href="">Ver Certificado</a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { Certifications };
