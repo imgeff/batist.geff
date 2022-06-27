a5b0c29bc30d7f300377b9f975c52909ee5c7f3a
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-06-27 12:22:17 -0300

    :construction: Atualiza estrutura de modal

--- src/components/Learnings/Modal/index.tsx
+++ src/components/Learnings/Modal/index.tsx
@@ -1,16 +1,26 @@
+import { Question, Star } from 'phosphor-react';
 import { IModalProps } from '../interfaces/Props';
 import './style.css';
 
-function Modal({ icon, title, description }: IModalProps) {
+function Modal({ icons, title, level, levelTip, description, iconCurrentColor }: IModalProps) {
+  const NEUTRAL400 = '#a3a3a3';
+
   return(
     <>
-      <input type="checkbox" id="modal-check" className="daisy-modal-toggle" />
-      <label htmlFor="modal-check" className="learning-modal daisy-modal">
-        <label className="learning-modal-box daisy-modal-box" htmlFor="">
+      <input type="checkbox" id={`check-${title}`} className="daisy-modal-toggle" />
+      <label htmlFor={`check-${title}`} className="learning-modal daisy-modal">
+        <label style={ { borderColor: iconCurrentColor }} className="learning-modal-box daisy-modal-box" htmlFor="">
           <figure className="learning-icon">
-            { icon }
+            { icons.defaultIcon }
           </figure>
-          <h3>{ title }</h3>
+          <h3 style={ { color: iconCurrentColor }}>{ title }</h3>
+          <span>
+            <Star size={12} weight="fill" color={ NEUTRAL400 }/>
+            <span>Nível: <strong style={ { color: iconCurrentColor }}>{ level }</strong></span>
+            <span className="daisy-tooltip z-10" data-tip={ levelTip }>
+              <Question size={18} weight="duotone" color={ NEUTRAL400 } />
+            </span>
+          </span>
           <p>{ description }</p>
         </label>
       </label>
