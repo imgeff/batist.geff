9464276827c87ed1389235189b95f437a4ec7884
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-28 00:07:22 -0300

    :iphone: feat: Aplica estilos para telas médias

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import './style.css';
 import ImgeffLogo from '../../images/IMGEFF.png';
+import { List, X } from 'phosphor-react';
+import { useState } from 'react';
 
 function Header () {
+  const [showMenu, setShowMenu] = useState(false);
 	return (
 		<header id='header'>
 			<img src={ ImgeffLogo } alt="Logo com a escrita IMGEFF" />
@@ -14,6 +17,36 @@ function Header () {
 					<li><a className='nav-link' href="#projects">Projetos</a></li>
 				</ul>
 			</nav>
+      <nav className='navbar-mobile'>
+        { showMenu ?  (
+            <X
+              size={40}
+              weight="fill"
+              color="#10b981"
+              onClick={ () => setShowMenu(!showMenu) }
+            />
+          ) : (
+            <List
+              size={40}
+              weight="fill"
+              color="#10b981"
+              onClick={ () => setShowMenu(!showMenu) }
+            />
+          )
+        }
+        { showMenu && (
+            <div className="links">
+              <ul className="menu-list">
+                <li onClick={ () => setShowMenu(false) } ><a className='nav-link' href="#presentation">Início</a></li>
+                <li onClick={ () => setShowMenu(false) } ><a className='nav-link' href="#about">Sobre</a></li>
+                <li onClick={ () => setShowMenu(false) } ><a className='nav-link' href="#certifications">Formação</a></li>
+                <li onClick={ () => setShowMenu(false) } ><a className='nav-link' href="#learnings">Habilidades</a></li>
+                <li onClick={ () => setShowMenu(false) } ><a className='nav-link' href="#projects">Projetos</a></li>
+              </ul>
+            </div>
+          )
+        }
+      </nav>
 		</header>
 	);
 }
